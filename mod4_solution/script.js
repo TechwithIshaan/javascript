cbfdfde059a5cc2363c7e0f259720e09979965f9
TechwithIshaan <71030723+TechwithIshaan@users.noreply.github.com>
2023-08-05 12:03:25 +0530

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,16 +1,9 @@
-(function () {
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
-  }
-  else {
-    helloSpeaker(names[i]);
-  }
-}
-
-})();
+})(window);
